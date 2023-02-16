63ca0820793c1a6e033d71e3125fae24860bbc68
Simon Michael <simon@joyful.com>
2023-02-16 11:09:54 -1000

    --print=v3: print >= 0 as >=

--- src/Print.hs
+++ src/Print.hs
@@ -6,7 +6,8 @@ import Import
 import Types
 
 -- | Print a shell test. See CLI documentation for details.
--- For v3 (the preferred, lightweight format), avoid printing unnecessary delimiters.
+-- For v3 (the preferred, lightweight format), avoid printing most unnecessary things
+-- (stdout delimiter, 0 exit status value).
 printShellTest
   :: String               -- ^ Shelltest format. Value of option @--print[=FORMAT]@.
   -> ShellTest            -- ^ Test to print
@@ -21,7 +22,7 @@ printShellTest format ShellTest{command=c,stdin=i,comments=comments,trailingComm
       printStdin "<<<" i
       printStdouterr False ">>>" o_expected
       printStdouterr False ">>>2" e_expected
-      printExitStatus True ">>>=" x_expected
+      printExitStatus True True ">>>=" x_expected
       printComments trailingComments
     "v2" -> do
       printComments comments
@@ -29,7 +30,7 @@ printShellTest format ShellTest{command=c,stdin=i,comments=comments,trailingComm
       printCommand "$$$ " c
       printStdouterr True ">>>" o_expected
       printStdouterr True ">>>2" e_expected
-      printExitStatus trailingblanklines ">>>=" x_expected
+      printExitStatus trailingblanklines True ">>>=" x_expected
       printComments trailingComments
     "v3" -> do
       printComments comments
@@ -37,7 +38,7 @@ printShellTest format ShellTest{command=c,stdin=i,comments=comments,trailingComm
       printCommand "$ "  c
       printStdouterr True ">" o_expected
       printStdouterr True ">2" e_expected
-      printExitStatus trailingblanklines ">=" x_expected
+      printExitStatus trailingblanklines False ">=" x_expected
       printComments trailingComments
     _ -> fail $ "Unsupported --print format: " ++ format
   where
@@ -62,7 +63,7 @@ printCommand prefix (FixedCommand s)       = printf "%s %s\n" prefix s
 -- If no expected value is specified, print nothing if first argument is true
 -- (for format 1, which ignores unspecified out/err), otherwise print a dummy test.
 printStdouterr :: Bool -> String -> Maybe Matcher -> IO ()
-printStdouterr alwaystest _ Nothing                      = when alwaystest $ printf "%s //\n"
+printStdouterr alwaystest prefix Nothing                 = when alwaystest $ printf "%s //\n" prefix
 printStdouterr _ _ (Just (Lines _ ""))                   = return ()
 printStdouterr _ _ (Just (Numeric _))                    = fail "FATAL: Cannot handle Matcher (Numeric) for stdout/stderr."
 printStdouterr _ _ (Just (NegativeNumeric _))            = fail "FATAL: Cannot handle Matcher (NegativeNumeric) for stdout/stderr."
@@ -70,10 +71,11 @@ printStdouterr _ prefix (Just (Lines _ s)) | prefix==">" = printf "%s" s  -- omi
 printStdouterr _ prefix (Just (Lines _ s))               = printf "%s\n%s" prefix s
 printStdouterr _ prefix (Just regex)                     = printf "%s %s\n" prefix (show regex)
 
--- | Print an expected exit status clause, unless the default (zero) is expected.
--- Always print if a true first argument is provided.
-printExitStatus :: Bool -> String -> Matcher -> IO ()
-printExitStatus _ _ (Lines _ _) = fail "FATAL: Cannot handle Matcher (Lines) for exit status."
-printExitStatus False _     (Numeric "0") = return ()
-printExitStatus True prefix (Numeric "0") = printf "%s 0\n" prefix
-printExitStatus _ prefix s = printf "%s %s\n" prefix (show s)
+-- | Print an expected exit status clause, prefixed with the given delimiter.
+-- If zero is expected:
+--  if the first argument is not true, nothing will be printed;
+--  otherwise if the second argument is not true, only the delimiter will be printed.
+printExitStatus :: Bool -> Bool ->  String -> Matcher ->      IO ()
+printExitStatus    _       _        _         (Lines _ _)   = fail "FATAL: Cannot handle Matcher (Lines) for exit status."
+printExitStatus    always  showzero prefix    (Numeric "0") = when always $ printf "%s %s\n" prefix (if showzero then "0" else "")
+printExitStatus    _       _        prefix    s             = printf "%s %s\n" prefix (show s)
