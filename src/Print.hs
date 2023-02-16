a6124798d0a1f51c13fc3a0ce75a365e07e1404a
Simon Michael <simon@joyful.com>
2023-02-16 07:47:53 -1000

    --print: always add exit delimiter if out/err ends with blank lines

--- src/Print.hs
+++ src/Print.hs
@@ -1,6 +1,7 @@
 module Print
 where
 
+import Safe (lastMay)
 import Import
 import Types
 
@@ -13,32 +14,37 @@ printShellTest
 printShellTest format ShellTest{command=c,stdin=i,comments=comments,trailingComments=trailingComments,
                stdoutExpected=o_expected,stderrExpected=e_expected,exitCodeExpected=x_expected}
                = do
-          case format of
-            "v1" -> do
-              printComments comments
-              printCommand "" c
-              printStdin "<<<" i
-              printStdouterr ">>>" o_expected
-              printStdouterr ">>>2" e_expected
-              printExitStatus True ">>>=" x_expected
-              printComments trailingComments
-            "v2" -> do
-              printComments comments
-              printStdin "<<<" i
-              printCommand "$$$ " c
-              printStdouterr ">>>" o_expected
-              printStdouterr ">>>2" e_expected
-              printExitStatus False ">>>=" x_expected
-              printComments trailingComments
-            "v3" -> do
-              printComments comments
-              printStdin "<" i
-              printCommand "$ "  c
-              printStdouterr ">" o_expected
-              printStdouterr ">2" e_expected
-              printExitStatus False ">=" x_expected
-              printComments trailingComments
-            _ -> fail $ "Unsupported --print format: " ++ format
+  case format of
+    "v1" -> do
+      printComments comments
+      printCommand "" c
+      printStdin "<<<" i
+      printStdouterr ">>>" o_expected
+      printStdouterr ">>>2" e_expected
+      printExitStatus True ">>>=" x_expected
+      printComments trailingComments
+    "v2" -> do
+      printComments comments
+      printStdin "<<<" i
+      printCommand "$$$ " c
+      printStdouterr ">>>" o_expected
+      printStdouterr ">>>2" e_expected
+      printExitStatus trailingblanklines ">>>=" x_expected
+      printComments trailingComments
+    "v3" -> do
+      printComments comments
+      printStdin "<" i
+      printCommand "$ "  c
+      printStdouterr ">" o_expected
+      printStdouterr ">2" e_expected
+      printExitStatus trailingblanklines ">=" x_expected
+      printComments trailingComments
+    _ -> fail $ "Unsupported --print format: " ++ format
+  where
+    trailingblanklines = case (o_expected, e_expected) of
+      (Just (Lines _ o), Just (Lines _ e)) -> hasblanks $ if null e then o else e
+      _ -> False
+      where hasblanks s = maybe False null $ lastMay $ lines s
 
 printComments :: [String] -> IO ()
 printComments = mapM_ putStrLn
@@ -61,7 +67,8 @@ printStdouterr prefix (Just (Lines _ s)) | prefix==">" = printf "%s" s  -- omit 
 printStdouterr prefix (Just (Lines _ s))    = printf "%s\n%s" prefix s
 printStdouterr prefix (Just regex)          = printf "%s %s\n" prefix (show regex)
 
--- | Print exit status. First arg says 'alwaysPrintEvenIfZero'.
+-- | Print expected exit status, unless the default (zero) is expected.
+-- Always print if a true first argument is provided.
 printExitStatus :: Bool -> String -> Matcher -> IO ()
 printExitStatus _ _ (Lines _ _) = fail "FATAL: Cannot handle Matcher (Lines) for exit status."
 printExitStatus False _     (Numeric "0") = return ()
